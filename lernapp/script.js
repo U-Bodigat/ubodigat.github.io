f67fda35ddd9454204173d498cfb94d32b22b1cf
U:Bodigat <89653017+Bodigat@users.noreply.github.com>
2022-12-07 03:48:36 +0100

    Add files via upload

---
+++ lernapp/script.js
@@ -0,0 +1,44 @@
+let dictionary = JSON.parse(localStorage.getItem('dictionary')) || {
+
+};
+let ran_key;
+
+function übunghinuzfügen() {
+    dictionary[Frage.value] = input2.value;
+
+    Frage.value = "";
+    input2.value = "";
+
+    localStorage.setItem('dictionary', JSON.stringify(dictionary));
+    render();
+}
+
+function render() {
+    Übungsliste.innerHTML = '';
+    for (let key in dictionary) {
+        Übungsliste.innerHTML += `<li> <b id="Abtrennung">F r a g e :</b> &nbsp ${dictionary[key]} &nbsp <b id="Abtrennung">L ö s u n g 💡:</b> &nbsp ${key}</li>`;
+    }
+}
+
+function löschen() {
+    localStorage.clear();
+}
+
+function nächstübung() {
+    let obj_keys = Object.keys(dictionary);
+    ran_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
+    diefrage.innerHTML = `${dictionary[ran_key]} &nbsp?`;
+
+    Antwort.value = "";
+}
+
+function richtigfalsch() {
+    if (Antwort.value == ran_key) {
+        überprüfungstext.innerHTML = `Richtig (;`;
+    } else {
+        überprüfungstext.innerHTML =
+            `Das ist leider falsch ;(` +
+            `<div id="lösungstext"> <h4>Das wäre die Lösung gewesen:</h4> <br> ${ran_key} </div>`;
+    }
+    nächstübung();
+}
