bc38d5c729ecf13dea171c91357c2ff3dc2a708d
U:Bodigat <Bodigat2@gmail.com>
2023-01-22 19:57:53 +0100

    22.01.2023 | Update text

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ function copyText() { //Das script ist dafür verantwortlich das der Text wo def
         console.error('Fehler beim Kopieren des Texts: ', err);
     });
 }
+
+function copyText2() { //Das script ist dafür verantwortlich das der Text wo definiert ist in der zwischenablage gespeichert wird.
+    var text = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjixK2t_tr8AhWfSPEDHfaDDEAQFnoECAwQAw&url=https%3A%2F%2Fwww.coolblue.de%2Fberatung%2Frichtiges-ladegeraet-fuer-samsung-galaxy-s22-auswaehlen.html%23%3A~%3Atext%3DMit%2520dem%2520Samsung%252025%2520Watt%2Cdieser%2520Zeit%2520auf%252040%2520Prozent.&usg=AOvVaw3H27gyFtBRPav3Z09JB7yf";
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Text erfolgreich kopiert');
+    }, function(err) {
+        console.error('Fehler beim Kopieren des Texts: ', err);
+    });
+}
