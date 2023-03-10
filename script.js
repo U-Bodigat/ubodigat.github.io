bafbaa8ae1aa9691747429b0b3133ae34bcdc246
U:Bodigat <Bodigat2@gmail.com>
2023-03-10 01:21:25 +0100

    10.03.2023 | Update Menü (Design, Menüpunkte, usw)

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ function copyText2() { //Das script ist dafür verantwortlich das der Text wo de
 }
 
 //Sort für die copierung der Texte in die Zwischenablage bei der drückung des jeweiligen buttons.
+
+//Code für 1 x 1 Tabelle
+const tableSize = 10; // Tabelle bis zu 10x10
+const tableElement = document.getElementById("table");
+
+// Funktion zum Erstellen einer Tabellenzeile
+function createRow(rowNum) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let i = 1; i <= tableSize; i++) {
+        const cell = document.createElement("button");
+        cell.innerText = `${rowNum} x ${i}`;
+        cell.addEventListener("click", () => checkAnswer(rowNum * i));
+        row.appendChild(cell);
+    }
+    return row;
+}
+
+// Funktion zum Erstellen der gesamten Tabelle
+function createTable() {
+    for (let i = 1; i <= tableSize; i++) {
+        const row = createRow(i);
+        tableElement.appendChild(row);
+    }
+}
+
+// Funktion zum Überprüfen der Antwort
+function checkAnswer(answer) {
+    const input = prompt("Was ist das Ergebnis?");
+    if (input == answer) {
+        alert("Richtig!");
+    } else {
+        alert(`Falsch. Die richtige Antwort ist ${answer}.`);
+    }
+}
+
+createTable();
+
+//ende des codes der 1 x 1 Tabelle
