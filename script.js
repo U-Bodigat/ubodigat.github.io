9cf423035f2289cebeaa423ef9f7c83cf2896d91
U:Bodigat <Bodigat2@gmail.com>
2023-06-11 09:14:20 +0200

    11.06.2023 | script-änderungen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let docTitel = document.title;
 window, addEventListener("blur", () => {
-    document.title = "ubodigat.tk | Inaktiv 😫";
+    document.title = "ubodigat.tk | Inaktiv 💤";
 })
 window, addEventListener("focus", () => {
     document.title = docTitel;
@@ -9,7 +9,7 @@ window, addEventListener("focus", () => {
 // Dies sorgt dafür dass wenn man den tab ändern sich der titel ändert.
 
 function copyText() { //Das script ist dafür verantwortlich das der Text wo definiert ist in der zwischenablage gespeichert wird.
-    var text = "Name: Samsung Group Gründungsjahr: 1938 Gründer: Lee Byung-chull Hauptsitz: Seoul, Südkorea Branche: Elektronik, Technologie Mitarbeiter: circa 300.000 (2021) Umsatz: circa 220 Milliarden US-Dollar (2020) Produkte: Smartphones, Tablets, tragbare Geräte, Haushaltsgeräte, Fernseher, Computer, Speicherchips, Halbleiter, Biotechnologie, Finanzdienstleistungen. Trucker_Erik";
+    var text = "Dieser Text ist leider nicht konfiguriert, melden sie sich beim Webseite-Betrieber. Ihr U:Bodigat.tk Support Team: 'contact@ubodigat.tk'";
     navigator.clipboard.writeText(text).then(function() {
         console.log('Text erfolgreich kopiert');
     }, function(err) {
