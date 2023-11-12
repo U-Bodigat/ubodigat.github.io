7117bcd265f3a5d17c9693bb0e1f02673fcaae46
U:Bodigat <Bodigat2@gmail.com>
2023-11-12 19:22:42 +0100

    12.11.2023 | Update/Sicherheit Download Page

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ function copyText2() { //Das script ist dafür verantwortlich das der Text wo de
 }
 
 //Sort für die copierung der Texte in die Zwischenablage bei der drückung des jeweiligen buttons.
+
+function download() {
+    alert(
+        "Algorithm: SHA1,\nHash: 88F1107F184B6D0000FEF41D22E459E71915AEC6,"
+    );
+}
+
+function DEReferatTranssexualität() {
+    alert(
+        "Algorithm: SHA1,\nHash: 0A1225CBA42DC4E17DFB7BFC4AB2D67D63644369,"
+    );
+}
+
+function DEReferatFeuerwehr() {
+    alert(
+        "Algorithm: SHA1,\nHash: AA43524BE18E6A9FE0D528EDA69ED9AFAB4432A8,"
+    );
+}
+
+function DEReferatRettungsdienst() {
+    alert(
+        "Algorithm: SHA1,\nHash: F1215756F620CD3385AEB014FB40C689599499FF,"
+    );
+}
+
+function LosAngelesVideoPhilipp() {
+    alert(
+        "Algorithm: SHA1,\nHash: 25B69831F86FD208C7FEB8CFB166955A0FBCEB66,"
+    );
+}
+
+function MSFSEDJAtoEDMKwithAirbusH135() {
+    alert(
+        "Algorithm: SHA1,\nHash: ,"
+    );
+}
+
+//Dieses schript zeigt im Download-beriech den Hash Prüfwert an
